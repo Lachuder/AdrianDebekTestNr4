5006fd86830468cb371aa871910efa0cfc5735fd
Adrian Debek <adryjano@gmail.com>
2026-03-07 17:50:30 +0100

    some changes...

--- src/main/java/zadanie1/StringContainer.java
+++ src/main/java/zadanie1/StringContainer.java
@@ -1,16 +1,52 @@
 package zadanie1;
 
+import java.time.LocalDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringContainer {
 
-    private String pattern;
-    private int index;
+    private String text;
+    private Pattern pattern;
+    private Node first;
+    private Node last;
+    private int size = 0;
 
-    public StringContainer(String pattern) {
-        this.pattern = pattern;
+    public StringContainer(String text) {
+        this.text = text;
     }
 
-    public void add(String text) {
+    public void add(String stringToBeAdded) {
+        if(!check(stringToBeAdded)) {
+            throw new InvalidStringContainerValueException("Bad Value");
+        } else if (duplicateNotAllowed) {
+            throw new DuplicatedElementOnListException("Duplicate not allowed");
+        } else {
+            Node newNode = new Node(stringToBeAdded);
+            if (first == null) {
+                first = newNode;
+            } else {
+                Node current = first;
+                while (current.next != null) {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+    }
 
+    private void addNode() {
+
+    }
+
+    private boolean check(String toBeChecked) {
+        Pattern pattern = Pattern.compile(text);
+        Matcher matcher = pattern.matcher(toBeChecked);
+        if (!matcher.matches()) {
+            throw new InvalidStringContainerPatternException("Bad Pattern");
+        } else {
+            return matcher.matches();
+        }
     }
 
     public int size() {
@@ -19,6 +55,16 @@ public class StringContainer {
 
     public String get(int index) {
 
+    }
+
+    static class Node {
+
+        String value;
+        Node next;
+
+        public Node(String value) {
+            this.value = value;
+        }
     }
 
 }
