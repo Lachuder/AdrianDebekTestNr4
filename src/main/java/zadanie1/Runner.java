dcd9c9e8967ebed53e49ec171abcc815926dd093
Adrian Debek <adryjano@gmail.com>
2026-03-07 16:53:04 +0100

    new classes: Runner, StringContainer

---
+++ src/main/java/zadanie1/Runner.java
@@ -0,0 +1,20 @@
+package zadanie1;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        StringContainer st = new StringContainer("\\d{2}[-]\\d{3}");
+
+        st.add("02-495");//git
+        st.add("01-120");//git
+        st.add("05-123");//git
+        st.add("00-000");//git
+//st.add("ala ma kota"); //powinno sie wywalic wyjatkiem InvalidStringContainerValueException(badValue)
+        for (int i = 0; i < st.size(); i++) {
+            System.out.println(st.get(i)); //powinno wypisac dodane kody pocztowe
+        }
+
+    }
+
+}
---
+++ src/main/java/zadanie1/StringContainer.java
@@ -0,0 +1,15 @@
+package zadanie1;
+
+public class StringContainer {
+
+    private String pattern;
+
+    public StringContainer(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public static void add() {
+
+    }
+
+}
