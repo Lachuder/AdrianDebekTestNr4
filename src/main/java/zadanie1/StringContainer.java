0d7d25fed219515c0cbee12486b21460079ad57a
Adrian Debek <adryjano@gmail.com>
2026-03-07 17:00:31 +0100

    added individual Exceptions

--- src/main/java/zadanie1/StringContainer.java
+++ src/main/java/zadanie1/StringContainer.java
@@ -3,12 +3,21 @@ package zadanie1;
 public class StringContainer {
 
     private String pattern;
+    private int index;
 
     public StringContainer(String pattern) {
         this.pattern = pattern;
     }
 
-    public static void add() {
+    public void add(String text) {
+
+    }
+
+    public int size() {
+
+    }
+
+    public String get(int index) {
 
     }
 
