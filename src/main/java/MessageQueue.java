3f7cdee93a774bd223969fa349def16a579acc0b
Adrian Debek <adryjano@gmail.com>
2026-03-07 12:51:26 +0100

    created MessageQueue.class for handling new coming messages with add() and take()

---
+++ src/main/java/MessageQueue.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * klasa przechowująca kolejkę wiadomości*/
+public class MessageQueue {
+
+    /**
+     * kolejka w bezpiecznej wielowątkowo implementacji (LinkedBlockingQueue - operacje mogą czekać) przechowująca
+     * kolejno dodawane elementy/wiadomości*/
+    private final BlockingQueue<String> queue = new LinkedBlockingQueue<String>();
+
+    /**
+     * metoda dodająca element/wiadomość do kolejki*/
+    public void add(String message) {
+        queue.add(message);
+    }
+
+    /**
+     * metoda pobierająca najstarszy element/wiadomość z kolejki
+     * rzuca wyjątek przerwania, gdy oczekiwanie zostanie naruszone*/
+    public String take() throws InterruptedException {
+        return queue.take();
+    }
+
+
+
+
+}
