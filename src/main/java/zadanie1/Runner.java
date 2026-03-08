684693ae598a016c8e0986cb241e59fdbcff2881
Adrian Debek <adryjano@gmail.com>
2026-03-08 17:47:37 +0100

    huge amount of work with late realization about no need of Iterator implementation

--- src/main/java/zadanie1/Runner.java
+++ src/main/java/zadanie1/Runner.java
@@ -4,16 +4,19 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        StringContainer st = new StringContainer("\\d{2}[-]\\d{3}");
+        StringContainer st = new StringContainer("\\d{2}[-]\\d{3}", true);
 
         st.add("02-495");//git
         st.add("01-120");//git
         st.add("05-123");//git
         st.add("00-000");//git
+//        st.add("00-000");//git
 //st.add("ala ma kota"); //powinno sie wywalic wyjatkiem InvalidStringContainerValueException(badValue)
         for (int i = 0; i < st.size(); i++) {
             System.out.println(st.get(i)); //powinno wypisac dodane kody pocztowe
         }
+
+        System.out.println(st.size());
 
     }
 
--- src/main/java/zadanie1/StringContainer.java
+++ src/main/java/zadanie1/StringContainer.java
@@ -1,45 +1,48 @@
 package zadanie1;
 
-import java.time.LocalDateTime;
+import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class StringContainer {
+public class StringContainer  {
 
     private String text;
     private Pattern pattern;
     private Node first;
-    private Node last;
+//    private Node last;
     private int size = 0;
+    private boolean duplicatesNotAllowed;
 
     public StringContainer(String text) {
         this.text = text;
     }
 
+    public StringContainer(String text, boolean duplicatesNotAllowed) {
+        this.text = text;
+        this.duplicatesNotAllowed = duplicatesNotAllowed;
+    }
+
     public void add(String stringToBeAdded) {
-        if(!check(stringToBeAdded)) {
+        if (!checkPattern(stringToBeAdded)) {
             throw new InvalidStringContainerValueException("Bad Value");
-        } else if (duplicateNotAllowed) {
-            throw new DuplicatedElementOnListException("Duplicate not allowed");
-        } else {
-            Node newNode = new Node(stringToBeAdded);
-            if (first == null) {
-                first = newNode;
-            } else {
-                Node current = first;
-                while (current.next != null) {
-                    current = current.next;
-                }
-                current.next = newNode;
-            }
         }
+        Node newNode = new Node(stringToBeAdded); //tworzymy nowy obiekt węzła z wartością podaną w argumencie
+        if (first == null) { //jeżeli jeszcze nic nie ma i pierwszy element jest pusty
+            first = newNode; //to pierwszym ustawiamy nowo stworzony węzeł
+        } else { //w przypadku obecności pierwszego elementu
+            if (duplicatesNotAllowed && isDuplicate(stringToBeAdded)) {
+                throw new DuplicatedElementOnListException("Duplicate not allowed");
+            }
+            Node temp = first; //przypisujemy jego wartość do tymczasowej zmiennej lokalnej jako punkt startu
+            while (temp.next != null) { //dopóki przypisany element ma jakiś następny
+                temp = temp.next; //to nadpisujemy tym następnym ten tymczasowy
+            }
+            temp.next = newNode; //a jak w końcu trafimy na nulla w miejscu następcy ostatniego elementu, to wstawimy naszego Noda
+        }
+        size++;
     }
 
-    private void addNode() {
-
-    }
-
-    private boolean check(String toBeChecked) {
+    private boolean checkPattern(String toBeChecked) {
         Pattern pattern = Pattern.compile(text);
         Matcher matcher = pattern.matcher(toBeChecked);
         if (!matcher.matches()) {
@@ -49,13 +52,92 @@ public class StringContainer {
         }
     }
 
-    public int size() {
+    private boolean isDuplicate(String toBeChecked) {
+        Node temp = first;
 
+        while (temp != null) {
+            if (temp.value.equals(toBeChecked)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
     }
 
     public String get(int index) {
+        if (index > size) {
+            throw new RuntimeException("Index above max");
+        }
 
+        int counter = 0;
+        Node temp = first;
+        while (counter < index) {
+            temp = temp.next;
+            counter++;
+        }
+        return temp.value;
     }
+
+//    @Override
+//    public Iterator<String> iterator() {
+//        return new StringIterator();
+//    }
+//
+//    private class StringIterator implements Iterator<String> {
+//
+//        Node current = first;
+//        Node previous = null;
+//        Node lastReturned = null;
+//        Node lastReturnedPrevious = null;
+//
+//        @Override
+//        public boolean hasNext() {
+//            return current != null;
+//        }
+//
+//        @Override
+//        public String next() {
+//
+//            if (!hasNext()) {
+//                throw new RuntimeException("empty");
+//            }
+//
+//            lastReturned = current;
+//            lastReturnedPrevious = previous;
+//
+//            String result = current.value;
+//
+//            previous = current;
+//            current = current.next;
+//
+//            return result;
+//        }
+//
+//        @Override
+//        public void remove() {
+//            if (lastReturned == null) {
+//                throw new IllegalStateException();
+//            }
+//
+//            if (lastReturnedPrevious == null) {
+//                first = first.next;
+//                if (first == null) {
+//                    last = null;
+//                }
+//            } else {
+//                lastReturnedPrevious.next = lastReturned.next;
+//                if (lastReturned == last) {
+//                    last = lastReturnedPrevious;
+//                }
+//            }
+//            previous = lastReturnedPrevious;
+//            lastReturned = null;
+//        }
+//    }
 
     static class Node {
 
@@ -68,3 +150,4 @@ public class StringContainer {
     }
 
 }
+
